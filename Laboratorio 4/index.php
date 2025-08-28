3dcae1de733d16163ddf7ce3c5fa5049ef821fa8
valeegrampin <valegrampin@gmail.com>
2025-08-28 11:49:19 -0300

    Update index.php

--- Laboratorio 4/index.php
+++ Laboratorio 4/index.php
@@ -1,1 +1,16 @@
-
+<?php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head><meta charset="UTF-8"><title>Laboratorio IV</title></head>
+<link rel="stylesheet" href="style.css">
+<body>
+<div class="container">
+<h1>Laboratorio IV</h1>
+<ul>
+  <li><a href="validar.php">Parte 1: Validar</a></li>
+  <li><a href="generar.php">Parte 2: Generar DV</a></li>
+</ul>
+</div>
+</body>
+</html>
