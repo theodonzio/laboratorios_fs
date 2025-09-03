288cd019b864e7b85353a99f87f57c3d344e6a9b
Thiago <thiagozabala92@gmail.com>
2025-09-03 08:48:55 -0300

    muchas cosas

---
+++ Laboratorio 6/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>labVI</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../Laboratorio 6/style.css">
+</head>
+<body>
+    <header>
+        <h1>LABORATORIO VI</h1>
+    </header>
+
+    <h2>Seleccione una app</h2>
+
+    <ul>
+        <li><img src="" alt="">Calculadora</li>
+        <li><img src="" alt="">Calculos geometricos</li>
+        <li><img src="" alt="">Ecuaciones cuadraticas</li>
+        <li><img src="" alt="">Tablas de multiplicar</li>
+        <li><img src="" alt="">5 de Oro</li>
+        <li><img src="" alt="">Factoriales</li>
+        <li><img src="" alt="">Bases numericas</li>
+        <li><img src="" alt="">Calculadora de Bases Numericas</li>
+        <li><img src="" alt="">Identificador de Cedulas</li>
+        <li><img src="" alt="">Digitos Verificadores</li>
+        <li><img src="" alt="">Ficha del Estudiante</li>
+    </ul>
+</body>
+</html>
