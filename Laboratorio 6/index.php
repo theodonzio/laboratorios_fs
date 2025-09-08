e9ec0235c1ecf50dc2fb884ac5baad92f45265bf
Thiago <thiagozabala92@gmail.com>
2025-09-08 11:03:10 -0300

    primer app

--- Laboratorio 6/index.php
+++ Laboratorio 6/index.php
@@ -7,27 +7,31 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="../Laboratorio 6/style.css">
 </head>
 <body>
     <header>
         <h1>LABORATORIO VI</h1>
     </header>
 
-    <h2>Seleccione una app</h2>
+    <main>
+    <div class="cartel">
+        <h2>Seleccione una app</h2>
+    </div>
 
     <ul>
-        <li><img src="" alt="">Calculadora</li>
-        <li><img src="" alt="">Calculos geometricos</li>
-        <li><img src="" alt="">Ecuaciones cuadraticas</li>
-        <li><img src="" alt="">Tablas de multiplicar</li>
-        <li><img src="" alt="">5 de Oro</li>
-        <li><img src="" alt="">Factoriales</li>
-        <li><img src="" alt="">Bases numericas</li>
-        <li><img src="" alt="">Calculadora de Bases Numericas</li>
-        <li><img src="" alt="">Identificador de Cedulas</li>
-        <li><img src="" alt="">Digitos Verificadores</li>
-        <li><img src="" alt="">Ficha del Estudiante</li>
+        <a href="../Laboratorio 1/operaciones.php"><li><img src="../img(apps)/icons8-calculator-64.png" alt="">Calculadora</li></a>
+        <li><img src="../img(apps)/icons8-surface-64.png" alt="">Calculos geometricos</li>
+        <li><img src="../img(apps)/icons8-equal-sign-64.png" alt="">Ecuaciones cuadraticas</li>
+        <li><img src="../img(apps)/icons8-multiply-64.png" alt="">Tablas de multiplicar</li>
+        <li><img src="../img(apps)/icons8-money-64.png" alt="">5 de Oro</li>
+        <li><img src="../img(apps)/icons8-exclamation-64.png" alt="">Factoriales</li>
+        <li><img src="../img(apps)/icons8-numbers-64.png" alt="">Bases numericas</li>
+        <li><img src="../img(apps)/icons8-calculator-64.png" alt="">Calculadora de Bases Numericas</li>
+        <li><img src="../img(apps)/icons8-id-64.png" alt="">Identificador de Cedulas</li>
+        <li><img src="../img(apps)/icons8-numbers-64.png" alt="">Digitos Verificadores</li>
+        <li><img src="../img(apps)/icons8-id-64 (1).png" alt="">Ficha del Estudiante</li>
     </ul>
+    </main>
 </body>
 </html>
